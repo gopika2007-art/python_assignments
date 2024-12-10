8f48efbd315a56a3999a6c6b45f758d1fc2b2069
Gopika Sivakumar <gsivakumar@freshworks.com>
2024-12-10 12:21:46 +0530

    value

--- practice2.py
+++ practice2.py
@@ -22,3 +22,31 @@ for x in fruits:
   if x == "banana":
     continue
   print(x)
+
+# prob 3
+def reverse_string(s):
+    return s[::-1]
+
+print(reverse_string("Python")) 
+
+# prob 4
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5))  
+
+# prob 5
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def prime_numbers_in_range(start, end):
+    return [num for num in range(start, end + 1) if is_prime(num)]
+
+print(prime_numbers_in_range(10, 20))  
